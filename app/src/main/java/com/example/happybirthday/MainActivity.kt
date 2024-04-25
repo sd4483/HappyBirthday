64fed2dda2bce5076f78a0c79f9f60bfccf5e4db
Sudheer <21117963+sd4483@users.noreply.github.com>
2024-04-25 23:10:14 +0100

    Align content Add text alignment properties to align message and from text to center and left, respectively

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -24,7 +29,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Sis!", from = "From Your brother")
+                    GreetingText(
+                        message = "Happy Birthday Sis!",
+                        from = "From Your brother",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
@@ -33,16 +42,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
             text = message,
             fontSize = 50.sp,
-            lineHeight = 60.sp
+            lineHeight = 60.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
             fontSize = 30.sp,
-            lineHeight = 45.sp
+            lineHeight = 45.sp,
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.Start),
         )
     }
 }
